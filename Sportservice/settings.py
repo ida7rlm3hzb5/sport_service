00f3e7683e16c78b5b0a87f2b23f0bf25e7aacc0
GrishinSV <svg95@list.ru>
2024-06-02 18:06:15 +0300

    static

--- Sportservice/settings.py
+++ Sportservice/settings.py
@@ -38,7 +38,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'mainapp.middleware.ExceptionHandlingMiddleware'
+    'mainapp.middleware.ExceptionHandlingMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'Sportservice.urls'
