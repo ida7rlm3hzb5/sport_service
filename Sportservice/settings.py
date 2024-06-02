d7488ad07c0a419ad19d28f9edc8e7690098a46e
GrishinSV <svg95@list.ru>
2024-06-02 18:04:02 +0300

    static

--- Sportservice/settings.py
+++ Sportservice/settings.py
@@ -107,10 +107,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / STATIC_URL,
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / STATIC_URL,
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
