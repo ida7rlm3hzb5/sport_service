ec4cd17061f0e64a0349bea3b487d786b07c43f4
GrishinSV <svg95@list.ru>
2024-06-06 00:25:20 +0300

    static

--- Sportservice/settings.py
+++ Sportservice/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATIC_ROOT = 'static/'
+STATIC_ROOT = 'static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / STATIC_URL,
